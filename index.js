5230375b4ad77189db485b12cac3ff18161554e7
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-09-17 13:13:37 +0100

    fixes

---
+++ index.js
@@ -0,0 +1,39 @@
+
+var btnProjectDescriptionSlider = document.querySelector("#slider-projDescription-btn");
+var btnGithubSlider = document.querySelector("#slider-github-btn");
+const sliderMap = {
+    "goblin-siege-slide" : new ProjectDetails("https://www.youtube.com/embed/gWUMZekq5qI?enablejsapi=1&mute=1", "https://github.com/shahilsaha05uk/GoblinSeige.git", "goblin-project"),
+
+    "strike-slide" : new ProjectDetails("https://www.youtube.com/embed/bxQbsuaC35c?si=psxg1fhxWueAv-Dl?enablejsapi=1&mute=1", "https://github.com/shahilsaha05uk/Year4_Multiplayer_Project.git", "strike-project"),
+
+    "antinal-island-slide" : new ProjectDetails("https://www.youtube.com/embed/gWUMZekq5qI?si=gx3txK7j8ejkxDUV?enablejsapi=1&mute=1", "https://github.com/shahilsaha05uk/AntinalIsland.git", "antinal-project")
+}
+
+// This will be executed when the page loads
+function init(){
+    var currentItemID = document.querySelector(".carousel-item.active iframe").id;
+    updateSliderBtnLinks(currentItemID);
+}
+
+function ProjectDetails(videoURL, githubLink, projectDescriptionID){
+    this.videoURL = videoURL;
+    this.githubLink = githubLink;
+    this.projectDescriptionID = projectDescriptionID;
+}
+
+function updateSliderBtnLinks(currentProjectID){
+    const projectDetails = sliderMap[currentProjectID];
+    if(projectDetails){
+        console.log(currentProjectID);
+        btnProjectDescriptionSlider.setAttribute("href", projectDetails.projectDescriptionID);
+        btnGithubSlider.setAttribute("href", projectDetails.githubLink);
+    }
+}
+
+
+document.getElementById('slider').addEventListener("slid.bs.carousel", function (event){
+    const activeID = event.relatedTarget.querySelector("iframe").id;
+    updateSliderBtnLinks(activeID);
+});
+
+init();
